870c4ee2576c7e18d85354ae245e17ca45c98168
Derek Leung <dltnf.alt@gmail.com>
2013-09-28 15:12:13 -0700

    admin enabled

--- dragonsmash/dragonsmash/urls.py
+++ dragonsmash/dragonsmash/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 
+from django.contrib import admin
+
 from rest_framework import routers
 from api import views
+
+admin.autodiscover()
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -20,7 +24,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+                       
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
