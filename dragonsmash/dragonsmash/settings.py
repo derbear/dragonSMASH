10ecec6fdeff45059d702769e53c4023d0403ddb
Derek Leung <dltnf.alt@gmail.com>
2013-09-28 15:00:49 -0700

    'api app and rest quickstart'

--- dragonsmash/dragonsmash/settings.py
+++ dragonsmash/dragonsmash/settings.py
@@ -127,6 +127,11 @@ INSTALLED_APPS = (
     'rest_framework'
 )
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGINATE_BY': 10
+}
+
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
 # A sample logging configuration. The only tangible logging
--- dragonsmash/dragonsmash/urls.py
+++ dragonsmash/dragonsmash/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import patterns, include, url
 
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,5 +21,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
